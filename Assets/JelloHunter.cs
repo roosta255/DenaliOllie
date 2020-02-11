2a0ee5067d1486fcfda37b86fab737ab158460e0
roosta255 <42880051+roosta255@users.noreply.github.com>
2020-02-10 20:54:22 -0600

    added_jello_hunters

---
+++ Assets/JelloHunter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JelloHunter : MonoBehaviour
+{
+    private CrunchyCrawl m_crunchyCrawl;
+
+    public Transform m_target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_crunchyCrawl = GetComponent<CrunchyCrawl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_target == null)
+            return;
+        
+        m_crunchyCrawl.m_destination = m_target.position;
+    }
+}
